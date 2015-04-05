a297312291351aa82c78ad210b09121bd5887643
taylor.fish <contact@taylor.fish>
2015-04-05 16:41:13 -0700

    Fix shellbot.py, fix formatting

--- shellbot.py
+++ shellbot.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 # Copyright (C) 2015 taylor.fish (https://github.com/taylordotfish)
 # Copyright (C) 2015 nc Krantz-Fire (https://pineco.net/)
-# 
+#
 # This file is part of Shellbot.
-# 
+#
 # Shellbot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Shellbot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Shellbot.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -41,17 +41,17 @@ class Shellbot(IrcBot):
         self.timeout = timeout
         self.prefix = prefix + " "
         self.allow_queries = allow_queries
-    
+
     def on_message(self, message, nickname, target, is_query):
-        if not message.startswith(prefix):
+        if not message.startswith(self.prefix):
             return
         if is_query and not self.allow_queries:
             return
-        
+
         print("[{0}] {1}: {2}".format(target, nickname, message))
-        lines = [x for x in Command(message[len(startstring):])
+        lines = [x for x in Command(message[len(self.prefix):])
             .run(self.timeout, self.timeout / 2) if x]
-        
+
         for line in lines[:self.max_lines]:
             self.send(target, line)
             print(">>> " + line)
@@ -69,7 +69,7 @@ def main():
     args = docopt(__doc__)
     bot = Shellbot(int(args["-m"]), float(args["-t"]),
         args["-p"], args["--queries"])
-    
+
     bot.connect(args["<host>"], int(args["<port>"]))
     bot.register(args["-n"])
     for channel in args["-c"]:
