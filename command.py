64df6cf8dc311440984d38de50982bd7d0f2f472
nickolas360 <nickolas360@users.noreply.github.com>
2015-06-07 18:45:04 -0700

    Relicense under AGPL, add help command

--- command.py
+++ command.py
@@ -3,17 +3,19 @@
 # This file is part of shellbot.
 #
 # shellbot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
 # shellbot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with shellbot.  If not, see <http://www.gnu.org/licenses/>.
+#
+# See EXCEPTIONS for additional permissions.
 
 from subprocess import call, Popen, PIPE, TimeoutExpired
 import os
